a730becaac498fe5ed5f921053ae7118dddc4628
Rodolfo Figueroa <ficr233@gmail.com>
2025-06-29 23:45:38 -0600

    se agrego el archivo ejerecicios en la carpeta Dia 5, ade,as del proyecto final del dia, solo falta corregir bugs a la hora de terminar el juego

---
+++ Día 5/ahorcado.py
@@ -0,0 +1,67 @@
+from random import choice
+
+lista_palabras = ['manzana', 'helado', 'carpinteria', 'escuela', 'mochila']
+
+def escoger_palabra(lista):
+    return choice(lista)
+
+def pedir_letra():
+    letra = input('Introduce una letra: ')
+    while(len(letra) != 1 or not letra.isalpha()):
+        letra = input('Introduce una letra válida (solo una): ')
+    return letra
+
+def buscar_letra(letra, palabra):
+    return palabra.count(letra)
+
+def buscar_indices(letra_seleccionada):
+    lista_indices = []
+    index = 0
+
+    for letra in palabra:
+        if letra_seleccionada == letra:
+            lista_indices.append(index)
+
+        index += 1
+
+    return lista_indices
+
+def mostrar_letras(indices, palabra_escondida, letra):
+    lista_escondida = list(palabra_escondida)
+    for indice in indices:
+        lista_escondida[indice] = letra
+
+    lista_escondida = ''.join(lista_escondida)
+    return lista_escondida
+
+
+print("########   Bienvenido al Juego del Ahorcado   ########")
+palabra = escoger_palabra(lista_palabras)
+palabra_oculta = palabra.replace(palabra, '-' * len(palabra))
+print(palabra_oculta)
+letras_erroneas = []
+vidas = 6
+
+while vidas > 0:
+    letra = pedir_letra()
+    letras_encontradas = buscar_letra(letra, palabra)
+
+    if letras_encontradas > 0:
+        indices = buscar_indices(letra)
+        palabra_oculta = mostrar_letras(indices, palabra_oculta, letra)
+        print(palabra_oculta)
+
+        if palabra_oculta == palabra:
+            print('Ganaste!')
+            break
+
+    elif vidas == 0:
+        print('Perdiste!')
+        break
+
+    else:
+        vidas -= 1
+        print(f'Fallaste, te quedan {vidas} vidas.')
+        letras_erroneas.append(letra)
+        print(f'Letras erroneas: {letras_erroneas}')
+        print(palabra_oculta)
--- Día 5/ejercicios.py
+++ Día 5/ejercicios.py
@@ -3,6 +3,8 @@
 #Si la suma de los 3 numeros es mayor a 15, va a devolver el numero mayor
 #Si la suma de los 3 numeros es menor a 10, va a devolver el numero menor
 #Si la suma de los tres numeros es un valor entre 10 y 15 incluidos va a devolver el numero de valor intermedio
+from itertools import count
+
 
 def devolver_distintos(*args):
     suma_total = sum(args)
@@ -24,7 +26,7 @@ devolver_distintos(1,4,3)
 def letras_unicas(palabra):
     return sorted(set(palabra))
 
-resultado = letras_unicas('parangaricutirimicuaro')
+resultado = letras_unicas('cascarrabias')
 print(resultado)
 
 #Ejercicio3
@@ -52,6 +54,51 @@ def cero_consecutivo(*args):
 
     return False
 
-
 res = cero_consecutivo(8,9,4,3,0,4,7,0)
 print(res)
+
+
+#Solucion udemy
+def ceros_vecinos(*args):
+    contador = 0
+
+    for num in args:
+        if contador + 1 == len(args):
+            return False
+        elif args[contador] == 0 and args[contador+1] == 0:
+            return True
+        else:
+            contador += 1
+    return False
+
+#Ejercicio4
+#Escribe una funcion llamada contar_primos() que requiera un solo argumento numerico. Esta funcion va a mostrar en pantalla
+#todos los numeros primos existentes en el rango de cero hasta ese numero incluido, y va a devolver la cantidad de numeros
+#primos que encontro. Por convencion el 0 y el 1 no se consideran primos.
+
+
+def contar_primos(numero):
+    primos = [2]
+    iteracion = 3
+
+    if numero < 2:
+        return 0
+
+    while iteracion <= numero:
+
+        for n in range(3, iteracion, 2):
+            if iteracion % n == 0:
+                iteracion += 2
+                break
+
+        else:
+            primos.append(iteracion)
+            iteracion += 2
+
+    print(primos)
+    return len(primos)
+
+print(contar_primos(50))
+
+
+
